2e45d488f953c1ce949df767c16c6f8bdf202ae7
Neil <=>
2020-05-17 12:50:43 +0100

    Updated duplicate item check

--- To Do List/app/routes.py
+++ To Do List/app/routes.py
@@ -1,5 +1,8 @@
 from app import *
 from flask import render_template, url_for, redirect, session, request
+from random import choice
+import string
+
 @app.route('/', methods=['GET'])
 def index():
     if 'list' not in session:
@@ -9,6 +12,9 @@ def index():
 
 @app.route('/add-new-item', methods=['POST'])
 def update_list():
+    if request.form.get('listitem') in session['list']:
+        error = 'Item already in list'
+        return render_template('index.html', error=error, items=session['list'])
     current_list = session['list']
     current_list.append(request.form.get('listitem'))
     session['list'] = current_list
@@ -21,5 +27,4 @@ def delete(listitem):
     current_list.remove(listitem)
     session['list'] = current_list
 
-    
     return redirect(url_for('index'))
