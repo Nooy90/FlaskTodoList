98c541a936add60d8650cc3f7e73e22fc6fc23bb
Neil <=>
2020-05-17 12:10:17 +0100

    Updated Init - Removed DB and imports

--- To Do List/app/__init__.py
+++ To Do List/app/__init__.py
@@ -1,12 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-import datetime
-
+from flask import Flask
 
 app = Flask(__name__)
 app.debug = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://user:pass@server:port/database'
-db = SQLAlchemy(app)
 
 from app import routes
 from app import models
